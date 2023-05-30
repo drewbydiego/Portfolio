acd980b432f524e92ac6b4ada477812824cf9440
drewbydiego <diegoflo64411@gmail.com>
2023-05-29 18:01:11 -0600

    Adding resume to be downloaded and adding description to projects

--- modal.js
+++ modal.js
@@ -9,7 +9,7 @@ const p1 = document.getElementById('p1');
 p1.addEventListener('click', ()=>{
     modal.style.display = "block";
     modalTittle.innerHTML = "CC Templates tool";
-    modalP.innerHTML = "CC Templates tool project";
+    modalP.innerHTML = "The CC Templates Tool is a project developed using HTML, CSS, and JavaScript. It serves as a valuable tool for call center agents, providing them with a user-friendly interface to enhance their efficiency and productivity.";
     modalImg.src = "img/Portfolioimg/project1.png";
     url = "https://drewbydiego.github.io/TemplateTool/"
 });
@@ -18,7 +18,7 @@ const p2 = document.getElementById('p2');
 p2.addEventListener('click', ()=>{
     modal.style.display = "block";
     modalTittle.innerHTML = "Voice recognition";
-    modalP.innerHTML = "Voice recognition project";
+    modalP.innerHTML = "'Voice Recognition' is a dynamic project that leverages HTML, CSS, and JavaScript to create a captivating voice-controlled experience. With just a few clicks, users can effortlessly listen and repeat phrases, as well as have text from a textbox accurately read aloud.";
     modalImg.src = "img/Portfolioimg/project2.png";
     url = "https://drewbydiego.github.io/ReconocimientoDeVoz/"
 });
@@ -26,7 +26,7 @@ const p3 = document.getElementById('p3');
 p3.addEventListener('click', ()=>{
     modal.style.display = "block";
     modalTittle.innerHTML = "Contact list";
-    modalP.innerHTML = "Contact list project";
+    modalP.innerHTML = "The 'Contact List' project is a web application built using HTML, CSS, and vanilla JavaScript. It offers a convenient and intuitive interface for managing and organizing contact information. With the ability to add and store contact details in your browser's local storage, it provides a seamless and hassle-free experience for keeping track of important contacts. ";
     modalImg.src = "img/Portfolioimg/project3.png";
     url = "https://drewbydiego.github.io/AgendaContactos/"
 });
@@ -34,7 +34,7 @@ const p4 = document.getElementById('p4');
 p4.addEventListener('click', ()=>{
     modal.style.display = "block";
     modalTittle.innerHTML = "Simple Clock";
-    modalP.innerHTML = "Simple Clock project";
+    modalP.innerHTML = "The Clock project is a web application created with HTML, CSS, and vanilla JavaScript. It offers a versatile set of features to help you keep track of time efficiently. With a simple clock display, a convenient timer functionality, and a reminder option utilizing desktop notifications, the Clock project provides a practical and user-friendly interface. Stay on schedule and never miss important events with this handy time management tool.";
     modalImg.src = "img/Portfolioimg/project4.png";
     url = "https://drewbydiego.github.io/RELOJ/"
 });
@@ -42,7 +42,7 @@ const p5 = document.getElementById('p5');
 p5.addEventListener('click', ()=>{
     modal.style.display = "block";
     modalTittle.innerHTML = "Lenguage Translator";
-    modalP.innerHTML = "Lenguage Translator project";
+    modalP.innerHTML = "The 'Language Translator' project is a web application built with HTML, CSS, and vanilla JavaScript. It leverages an API to enable prompt translation from one language to another. With a user-friendly interface, this project serves as a valuable tool for efficient and productive language translation. Expand your global reach and overcome language barriers with the 'Language Translator' application.";
     modalImg.src = "img/Portfolioimg/project5.png";
     url = "https://drewbydiego.github.io/TranslateAPI/"
 });
